f7af96536fa50a5be9f7f3e64d35ac64417355d5
Zeffry Reynando <zeffry.reynando@gmail.com>
2023-12-17 09:39:09 +0700

    Remove unused column linkTask from ProjectTask table

---
+++ prisma/migrations/20231217023618_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `link_task` on the `project_tasks` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "project_tasks" DROP COLUMN "link_task";
